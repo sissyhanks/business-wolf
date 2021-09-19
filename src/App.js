b405cf856671812f200b90338ab73e922cb5f404
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-19 15:39:13 -0400

    set home page to render About

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Header from "./components/Header";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Footer from "./components/Footer";
+
 // The state of the app is based on the page selected from the navigation list. because the state needs to be referenced by several components it is added to the app.js file to make it global
 import { useState } from 'react'
 
 function App(){
-  const [currentPage, setCurrentPage] = useState('About');
+  const [currentPage] = useState('About');
 
   return (
     <div className="container">
+      <Header currentPage={currentPage} />
       <PortfolioContainer currentPage={currentPage} />
+      <Footer />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import NavTabs from './NavTabs'
 
-const Header = () => {
+const Header = ( {currentPage, setCurrentPage} ) => {
+  const handlePageChange = (page) => setCurrentPage(page);
   return (
     <div>
-      
+      <h1>Business Wolf</h1>
+            {/* We are passing the currentPage from state and the function to update it */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
     </div>
   )
 }
