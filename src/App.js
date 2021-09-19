9a5c39a48abee128a486d4940f0551cb94217e81
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-19 15:02:18 -0400

    set currentPage state to global

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+// The state of the app is based on the page selected from the navigation list. because the state needs to be referenced by several components it is added to the app.js file to make it global
+import { useState } from 'react'
 
-const App = () => <PortfolioContainer />;
+function App(){
+  const [currentPage, setCurrentPage] = useState('About');
+
+  return (
+    <div className="container">
+      <PortfolioContainer currentPage={currentPage} />
+    </div>
+  );
+}
 
 export default App;
